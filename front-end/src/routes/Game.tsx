7122b249116b99531f47f91e7e6d33ae3cac6bee
Carter Costic <cartercostic@gmail.com>
2023-02-22 22:06:33 -0500

    frontend work

--- front-end/src/routes/Game.tsx
+++ front-end/src/routes/Game.tsx
@@ -1,17 +1,17 @@
 import * as React from "react";
 import { Button, Grid, IconButton } from "@mui/material";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
-// import LeaderboardIcon from '@mui/icons-material/Leaderboard';
+import LeaderboardIcon from "@mui/icons-material/Leaderboard";
 import { useNavigate } from "react-router-dom";
-import SettingsDialog from "../components/SettingsDialog";
-import SettingsWheel from "../assets/SettingsWheel.svg";
 import GameLay from "../containers/GameLay";
 import Space from "../assets/Space/Space.svg";
+// import SettingsWheel from "../assets/SettingsWheel.svg";
 import MaskedText from "../components/MaskedText";
+import LeaderboardDialog from "../components/Game/LeaderboardDialog";
 
 const Game = () => {
   const navigate = useNavigate();
-  const [settingsOpen, setSettingsOpen] = React.useState(false);
+  const [leaderboardOpen, setLeaderboardOpen] = React.useState(false);
 
   return (
     <div>
@@ -24,12 +24,13 @@ const Game = () => {
             </Button>
           </Grid>
           <Grid item xs={6} sx={{ display: "flex", justifyContent: "end" }}>
-            <IconButton aria-label="Settings" onClick={() => setSettingsOpen(true)}>
-              <img src={SettingsWheel} alt="Settings Button" style={{ width: "3vw" }} />
+            <IconButton aria-label="Settings" onClick={() => setLeaderboardOpen(true)}>
+              {/* <img src={SettingsWheel} alt="Settings Button" style={{ width: "3vw" }} /> */}
+              <LeaderboardIcon fontSize="large" sx={{ color: "white" }} />
             </IconButton>
           </Grid>
         </Grid>
-        <SettingsDialog open={settingsOpen} handleClose={() => setSettingsOpen(false)} />
+        <LeaderboardDialog open={leaderboardOpen} handleClose={() => setLeaderboardOpen(false)} />
       </Grid>
       <GameLay />
     </div>
