3b29659b5e7ba8804611ebe3d9dce2438326446e
TharunTK <tharun.tiruppali@gmail.com>
2023-02-23 22:00:12 -0500

    added api file, need to integrate into leaderboard

--- back-end/openai_contacter.py
+++ back-end/openai_contacter.py
@@ -1,3 +1,4 @@
+from flask import request
 import openai
 import random
 
@@ -29,33 +30,66 @@ def chatgpt_response():
     player_money = inp['money']
     chatGPT_cards = inp['cards']
     current_community = inp['community']
-    prompt = 'Player 1 and Player 2 are playing texas holdem. Player 1 has $' + str(
-        player_money) + '. Player 1 has ' + get_string_card(
-        chatGPT_cards['cards'][0]) + ' and ' + get_string_card(chatGPT_cards['cards'][1]) + \
-             '. Player 2 has two unknown cards. There is a '
-    for card in current_community:
-        prompt += get_string_card(card)+","
+    bet = inp['bet']
+    if bet == 0:
+        prompt = 'Player 1 and Player 2 are playing texas holdem. Player 1 has $' + str(
+            player_money) + '. Player 1 has ' + get_string_card(
+            chatGPT_cards['cards'][0]) + ' and ' + get_string_card(chatGPT_cards['cards'][1]) + \
+                '. Player 2 has two unknown cards. There is a '
+        for card in current_community:
+            prompt += get_string_card(card)+","
 
-    prompt = prompt[0:-1] + ' on the table. How much exactly should Player 1 bet?\nBased on odds and calculations, Player 1 should bet specifically $'
+        prompt = prompt[0:-1] + ' on the table. How much exactly should Player 1 bet?\nBased on odds and calculations, Player 1 should bet specifically $'
 
-    completion = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=2,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
+        completion = openai.Completion.create(
+            engine=model_engine,
+            prompt=prompt,
+            max_tokens=2,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
 
-    response = completion.choices[0].text
-    curr_val = 0
-    for i in response:
-        try:
-            curr_val = curr_val * 10 + int(i)
-        except:
-            continue
+        response = completion.choices[0].text
+        curr_val = 0
+        for i in response:
+            try:
+                curr_val = curr_val * 10 + int(i)
+            except:
+                continue
 
-    if not (curr_val >= 0 and curr_val < player_money):
-        print('\nused random val \n')
-        curr_val = random.randrange(0, player_money)
-    return '\n$' + str(curr_val)
+        if not (curr_val >= 0 and curr_val < player_money):
+            print('\nused random val \n')
+            curr_val = random.randrange(0, player_money)
+        return str(curr_val)
+    
+    else:
+        prompt = 'Player 1 and Player 2 are playing texas holdem. Player 1 has $' + str(
+            player_money) + '. Player 1 has ' + get_string_card(
+            chatGPT_cards['cards'][0]) + ' and ' + get_string_card(chatGPT_cards['cards'][1]) + \
+                '. Player 2 has two unknown cards. There is a '
+        for card in current_community:
+            prompt += get_string_card(card)+","
+
+        prompt = prompt[0:-1] + ' on the table. Player 2 has bet $'+str(bet)+'How much exactly should Player 1 bet?\nBased on odds and calculations, Player 1 should bet specifically $'
+        
+        completion = openai.Completion.create(
+            engine=model_engine,
+            prompt=prompt,
+            max_tokens=2,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        response = completion.choices[0].text
+        curr_val = 0
+        for i in response:
+            try:
+                curr_val = curr_val * 10 + int(i)
+            except:
+                continue
+        if curr_val < bet:
+            return '-1'
+        return str(str(curr_val))
+
+    
