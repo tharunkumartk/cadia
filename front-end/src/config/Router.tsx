5b2a5d456230aabea4db4145636f000ebffc0453
Carter Costic <cartercostic@gmail.com>
2023-03-13 00:40:30 -0400

    quick router patch

--- front-end/src/config/Router.tsx
+++ front-end/src/config/Router.tsx
@@ -1,27 +1,27 @@
 import * as React from "react";
 
 import Landing from "../routes/Landing";
-import Home from "../routes/Home"
-import Game from "../routes/Game"
+import Home from "../routes/Home";
+import Game from "../routes/Game";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      { path: "/", element: <Landing /> },
-      { path: "/home", element: <Home /> },
-      {
-        path: "/game",
-        element: <Game />,
-      },
-      // {
-      //   path: "/contact-us",
-      //   element: <Contact />,
-      // },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//     children: [
+//       { path: "/", element: <Landing /> },
+//       { path: "/home", element: <Home /> },
+//       {
+//         path: "/game",
+//         element: <Game />,
+//       },
+//       // {
+//       //   path: "/contact-us",
+//       //   element: <Contact />,
+//       // },
+//     ],
+//   },
+// ]);
 
 interface RouteSchema {
   path: string;
