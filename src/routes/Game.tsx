bec8ccb1390fb685fdba639cb3c83cbbfee1299b
TharunTK <tharun.tiruppali@gmail.com>
2023-03-21 18:03:03 -0400

    feedback button on game page

--- src/routes/Game.tsx
+++ src/routes/Game.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button, Grid, IconButton } from "@mui/material";
+import { Button, Grid, IconButton, Typography } from "@mui/material";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import LeaderboardIcon from "@mui/icons-material/Leaderboard";
 import { useNavigate } from "react-router-dom";
@@ -22,7 +22,25 @@ const Game = () => {
               <MaskedText text="Back" fontSize="1rem" />
             </Button>
           </Grid>
-          <Grid item xs={6} sx={{ display: "flex", justifyContent: "end" }}>
+          <Grid item xs={5.5} sx={{ display: "flex", justifyContent: "end" }}>
+            <Button
+              variant="contained"
+              sx={{ backgroundColor: "#EB9BD5", margin: "10px" }}
+              onClick={() => window.open("https://forms.gle/FeUW8GK667nXqa7e8")}
+            >
+              <Typography
+                sx={{
+                  fontFamily: "Joystix",
+                  fontSize: "1rem",
+                  color: "white",
+                  // textShadow: "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black",
+                }}
+              >
+                Feedback
+              </Typography>
+            </Button>
+          </Grid>
+          <Grid item xs={0.5} sx={{ display: "flex", justifyContent: "end" }}>
             <IconButton aria-label="Settings" onClick={() => setLeaderboardOpen(true)}>
               {/* <img src={SettingsWheel} alt="Settings Button" style={{ width: "3vw" }} /> */}
               <LeaderboardIcon fontSize="large" sx={{ color: "white" }} />
