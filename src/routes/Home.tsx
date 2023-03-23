ac5e06a90f1f103266369f1ef8ff08e2cd17657e
Carter Costic <cartercostic@gmail.com>
2023-03-22 22:43:20 -0400

    remove feedback button from home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -37,22 +37,6 @@ const Home = () => {
                 Our Openfund
               </Typography>
             </Button>
-            <Button
-              variant="contained"
-              sx={{ backgroundColor: "#EB9BD5", margin: "10px" }}
-              onClick={() => window.open("https://forms.gle/FeUW8GK667nXqa7e8")}
-            >
-              <Typography
-                sx={{
-                  fontFamily: "Joystix",
-                  fontSize: "1rem",
-                  color: "white",
-                  // textShadow: "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black",
-                }}
-              >
-                Feedback
-              </Typography>
-            </Button>
           </Grid>
           <Grid item flexGrow={1} />
           <Grid item>
